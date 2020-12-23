c194b0e5dd96ad9e1fdac2c0fb6cc212e81657a5
ricardotenorio <ricardo.heber@gmail.com>
2020-12-23 02:15:14 -0300

    first commit

---
+++ src/main/java/ricardotenorio/reserva/de/livros/ReservaDeLivrosApplication.java
@@ -0,0 +1,13 @@
+package ricardotenorio.reserva.de.livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaDeLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaDeLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ricardotenorio/reserva/de/livros/ReservaDeLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package ricardotenorio.reserva.de.livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaDeLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
